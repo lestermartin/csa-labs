bf708fa210df1716dd174cf774a0e36fa4206881
Lester Martin <lester.martin@gmail.com>
2021-04-05 16:03:05 -0400

    Create WordCountFromSocket.java

---
+++ flink-exercises/wordcount/src/main/java/wc/WordCountFromSocket.java
@@ -0,0 +1,59 @@
+package wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class WordCountFromSocket
+{
+    public static void main(String[] args)
+            throws Exception
+    {
+        // boiler plate code
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        ParameterTool params = ParameterTool.fromArgs(args);
+        env.getConfig().setGlobalJobParameters(params);
+
+        // listen to a socket
+        DataStream<String> lines = env.socketTextStream("localhost", 9999);
+
+        //calculate word counts
+        DataStream<Tuple2<String, Integer>> wordcounts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                lines.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .keyBy(0).sum(1);
+
+        wordcounts.print();
+        env.execute("WordCount Sourced on Socket Example");
+    }
+
+    // *************************************************************************
+    //     USER FUNCTIONS
+    // *************************************************************************
+
+    /**
+     * Implements the string tokenizer that splits sentences into words as a user-defined
+     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
+     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
+     */
+    public static final class Tokenizer
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
+}
